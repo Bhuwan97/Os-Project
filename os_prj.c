922b8a60d9c7f1edf8470ba79cfa5fb28de8636c
Bhuwanesh <artc2006@gmail.com>
2018-03-25 23:09:16 +0530

    index added

--- os_prj.c
+++ os_prj.c
@@ -3,3 +3,10 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+int main()
+{
+  pthread_t ProcCurr[3][3]; /*id of thread*/
+  pthread_attr_t attr;
+  int  i, j;
+  long r1 = 1,r2 = 2,r3 = 3;
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 This is my Poject.
+Added matrix
