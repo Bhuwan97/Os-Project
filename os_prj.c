ad9bd3580fc533650420d2051daf80160d3c9cb9
Bhuwanesh <artc2006@gmail.com>
2018-03-30 21:14:01 +0530

    Third commit

--- os_prj.c
+++ os_prj.c
@@ -1,12 +1,37 @@
 #include<stdio.h>
 #include<pthread.h>
-#include<stdlib.h>
 #include<unistd.h>
+#include<stdlib.h>
+#define MaxP 10
+#define MaxT 10
 
-int main()
-{
-  pthread_t ProcCurr[3][3]; /*id of thread*/
-  pthread_attr_t attr;
-  int  i, j;
-  long r1 = 1,r2 = 2,r3 = 3;
+void *watch_count(void *r);
+int Available[10];
+int all[20][20];
+int max[20][20];
+int need[20][20];
+
+pthread_mutex_t mutex;
+
+
+int main(){
+    //All decleration.
+    int n,m;
+printf("Enter size of matrix n x m:");
+scanf("%d %d",&n,&m);
+printf("\n");
+printf("Enter values for allocation matrix:");
+int i,j;
+for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
+        scanf("%d",&all[i][j]);
+        printf("\n");
+  printf("Enter values for max demand matrix:");
+for(i=0;i<n;i++)
+    for(j=0;j<m;j++)
+        scanf("%d",&max[i][j]);
+       printf("\n");
+
+
+
 }
